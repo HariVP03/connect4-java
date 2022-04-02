24a83b6194edf5444827d28a34da3fef1bc0ee90
HariVP03 <harryskotch11@gmail.com>
2022-04-02 17:14:31 +0530

    made fixes

--- src/Game.java
+++ src/Game.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Game {
 
     static int[][] board = new int[7][6];
+    static int player = 1;
 
     public static void displayBoard() {
         for (int i = 0; i < 6; i++) {
@@ -18,27 +19,41 @@ public class Game {
         return board[x][y] != 0;
     }
 
-    public static void addTokenToCell(int x, int y, int player) {
-        if (player != 1 || player != 2) return;
+    public static void addTokenToCell(int x, int y) {
+        if (player != 1 && player != 2)
+            return;
         board[x][y] = player;
     }
 
-    public static void dropToken(int x, int player) {
+    public static void dropToken(int x) {
         boolean top = isOccupied(x, 0);
         if (top) return;
         int curr = 0;
         for (int i = 1; i < 6; i++) {
             if (isOccupied(x, i)) {
-                addTokenToCell(x, curr, player);
+                addTokenToCell(x, curr);
                 return;
             }
             curr++;
         }
-        addTokenToCell(x, 5, player);
+        addTokenToCell(x, 5);
     }
 
     private static void clearScreen() {
         for (int i = 0; i < 50; ++i) System.out.println();
+    }
+
+    private static void displayMenu() {
+        if (player == 1)
+            System.out.println("Enter the row (0-6) for player 1 or enter 9 to exit: ");
+        else if (player == 2)
+            System.out.println("Enter the row (0-6) for player 2 or enter 9 to exit: ");
+        else
+            System.out.println("Wrong Player");
+    }
+
+    private static void togglePlayer() {
+        player = player == 1 ? 2 : 1;
     }
 
     public static void main(String[] arg) {
@@ -48,14 +63,22 @@ public class Game {
 
     public void start() {
         Scanner sc = new Scanner(System.in);
-        System.out.println();
-        System.out.println("GAME STARTED!");
-        System.out.println();
-        displayBoard();
-        System.out.println("Enter the row (0-6) for player 1: ");
-        int x = sc.nextInt();
-        clearScreen();
-        displayBoard();
+        while (true) {
+            clearScreen();
+            displayBoard();
+            displayMenu();
 
+            int c = sc.nextInt();
+            System.out.println(c);
+            if (c >= 0 && c <= 6) {
+                System.out.println("dropped token");
+                dropToken(c);
+                System.out.println(board[0][5]);
+            } else if (c == 9) {
+                break;
+            } else
+                System.out.println("Invalid Input");
+            togglePlayer();
+        }
     }
 }
